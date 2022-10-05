baf4569864b491eeab45574184040b4ee5d3bcd8
unknown <manuel.eric.b@gmail.com>
2022-10-05 09:56:33 +0700

    Add migration for tbl major + tbl user educations

---
+++ database/migrations/table/2022_10_04_090207_create_tbl_user_educations_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_user_educations', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->unsignedBigInteger('univ_id')->nullable();
+            $table->foreign('univ_id')->references('id')->on('tbl_univ')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->unsignedBigInteger('major_id')->nullable();
+            $table->foreign('major_id')->references('id')->on('tbl_major')->onUpdate('cascade')->onDelete('cascade');
+            
+            $table->string('degree');
+            $table->date('graduation_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_user_educations');
+    }
+};
