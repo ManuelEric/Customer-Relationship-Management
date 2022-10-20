f7ad4834394a318c2d9637a11fc9fdbe3e9c42b7
hafidz <hafidz.bdt@gmail.com>
2022-10-20 12:00:18 +0700

    lead new

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Redirect;
 class LeadController extends Controller
 {
     use CreateCustomPrimaryKeyTrait;
-    
+
     private LeadRepositoryInterface $leadRepository;
 
     public function __construct(LeadRepositoryInterface $leadRepository)
@@ -49,7 +49,7 @@ class LeadController extends Controller
             $leadDetails['main_lead'] = "KOL";
         else
             $leadDetails['sub_lead'] = null;
-        
+
         $lead_id_without_label = $this->remove_primarykey_label($last_id, 2);
         $lead_id_with_label = 'LS' . $this->add_digit($lead_id_without_label + 1, 3);
 
@@ -58,7 +58,6 @@ class LeadController extends Controller
 
             $this->leadRepository->createLead(['lead_id' => $lead_id_with_label] + $leadDetails);
             DB::commit();
-
         } catch (Exception $e) {
 
             DB::rollBack();
@@ -75,6 +74,10 @@ class LeadController extends Controller
 
     public function edit(Request $request)
     {
+        if ($request->ajax()) {
+            return $this->leadRepository->getAllLeadDataTables();
+        }
+
         $leadId = $request->route('lead');
 
         # retrieve lead data by id
@@ -82,7 +85,7 @@ class LeadController extends Controller
         # put the link to update lead form below
         # example
 
-        return view('lead.form')->with(
+        return view('pages.lead.index')->with(
             [
                 'lead' => $lead
             ]
@@ -111,7 +114,6 @@ class LeadController extends Controller
 
             $this->leadRepository->updateLead($leadId, $leadDetails);
             DB::commit();
-
         } catch (Exception $e) {
 
             DB::rollBack();
--- resources/views/lead/form.blade.php
+++ resources/views/lead/form.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
     @if ($errors->any())
         <div class="alert alert-danger">
@@ -17,7 +19,9 @@
         </div>
     @endif
 
-    <form action="@if (isset($lead)) {{ '/master/lead/' . $lead->lead_id }}@else{{ '/master/lead' }} @endif" method="POST">
+    <form
+        action="@if (isset($lead)) {{ '/master/lead/' . $lead->lead_id }}@else{{ '/master/lead' }} @endif"
+        method="POST">
         @csrf
         @if (isset($lead))
             @method('put')
@@ -26,32 +30,36 @@
         <input type="hidden" name="id" value="{{ $lead->id }}">
 
         <div id="main_lead">
-            Lead name: <input type="text" name="main_lead" value="@if (isset($lead->main_lead) && ($lead->main_lead != "KOL")){{ $lead->main_lead }}@else{{ old('main_lead') }}@endif">
+            Lead name: <input type="text" name="main_lead"
+                value="@if (isset($lead->main_lead) && $lead->main_lead != 'KOL') {{ $lead->main_lead }}@else{{ old('main_lead') }} @endif">
             <br>
         </div>
 
         <div id="sub_lead" style="display: none">
-            Lead name: <input type="text" name="sub_lead" value="@if (isset($lead->sub_lead)){{ $lead->sub_lead }}@else{{ old('sub_lead') }}@endif">
+            Lead name: <input type="text" name="sub_lead"
+                value="@if (isset($lead->sub_lead)) {{ $lead->sub_lead }}@else{{ old('sub_lead') }} @endif">
             <br>
         </div>
 
         Score: <input type="number" name="score" value="{{ isset($lead->score) ? $lead->score : old('score') }}">
         <br>
-        KOL <input type="checkbox" name="kol" value="true" onchange="changeKOL()" @if (isset($lead->main_lead) && ($lead->main_lead == "KOL")){{ "checked" }}@endif>
+        KOL <input type="checkbox" name="kol" onchange="changeKOL()"
+            @if (isset($lead->main_lead) && $lead->main_lead == 'KOL') {{ 'checked' }} @endif>
         <br>
         <input type="submit" value="Submit">
 
     </form>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"
+        integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script type="text/javascript">
         $(document).ready(function() {
             changeKOL()
         })
 
-        function changeKOL()
-        {
+        function changeKOL() {
             var checked = $("input[name=kol]").prop('checked');
-                
+
             if (checked) {
                 $("#main_lead").hide();
                 $("#sub_lead").show();
@@ -62,4 +70,5 @@
         }
     </script>
 </body>
+
 </html>
