124e55cd1c922f3bc7096fb7b59a4c9022da42d1
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2022-12-01 10:45:27 +0700

    1/12/2022

--- resources/views/pages/program/referral/form.blade.php
+++ resources/views/pages/program/referral/form.blade.php
@@ -4,9 +4,9 @@
 
 @section('content')
 
-@php
-    $disabled = isset($referral) && isset($edit) ? null : (isset($edit) ? null : "disabled");
-@endphp
+    @php
+        $disabled = isset($referral) && isset($edit) ? null : (isset($edit) ? null : 'disabled');
+    @endphp
 
     <div class="d-flex align-items-center justify-content-between mb-3">
         <a href="{{ url('program/referral') }}" class="text-decoration-none text-muted">
@@ -23,17 +23,20 @@
                     @if (isset($referral))
                         <div class="mt-3 d-flex justify-content-center">
                             @if (empty($edit))
-                                <a href="{{ route('referral.edit', ['referral' => $referral->id]) }}" class="btn btn-sm btn-outline-info rounded mx-1">
+                                <a href="{{ route('referral.edit', ['referral' => $referral->id]) }}"
+                                    class="btn btn-sm btn-outline-info rounded mx-1">
                                     <i class="bi bi-pencil"></i> Edit
                                 </a>
                             @else
-                                <a href="{{ route('referral.show', ['referral' => $referral->id]) }}" class="btn btn-sm btn-outline-info rounded mx-1">
+                                <a href="{{ route('referral.show', ['referral' => $referral->id]) }}"
+                                    class="btn btn-sm btn-outline-info rounded mx-1">
                                     <i class="bi bi-arrow-left"></i>
                                     Back</a>
                             @endif
-                                <button type="button" class="btn btn-sm btn-outline-danger rounded mx-1" onclick="confirmDelete('program/referral', '{{ $referral->id }}')">
-                                    <i class="bi bi-trash2"></i> Delete
-                                </button>
+                            <button type="button" class="btn btn-sm btn-outline-danger rounded mx-1"
+                                onclick="confirmDelete('program/referral', '{{ $referral->id }}')">
+                                <i class="bi bi-trash2"></i> Delete
+                            </button>
                         </div>
                     @endif
                 </div>
@@ -50,7 +53,9 @@
                     </div>
                 </div>
                 <div class="card-body">
-                    <form action="{{ isset($referral) ? route('referral.update', ['referral' => $referral->id]) : route('referral.store') }}" method="POST">
+                    <form
+                        action="{{ isset($referral) ? route('referral.update', ['referral' => $referral->id]) : route('referral.store') }}"
+                        method="POST">
                         @csrf
                         @if (isset($referral))
                             @method('PUT')
@@ -58,10 +63,15 @@
                         <div class="row">
                             <div class="col-md-6 mb-2">
                                 <label>Type</label>
-                                <select name="referral_type" class="select w-100" id="type" onchange="checkType()" {{ $disabled }}>
+                                <select name="referral_type" class="select w-100" id="type" onchange="checkType()"
+                                    {{ $disabled }}>
                                     <option data-placeholder="true"></option>
-                                    <option value="In" {{ isset($referral->referral_type) && $referral->referral_type == "In" ? "selected" : null }}>Referral In</option>
-                                    <option value="Out" {{ isset($referral->referral_type) && $referral->referral_type == "Out" ? "selected" : null }}>Referral Out</option>
+                                    <option value="In"
+                                        {{ isset($referral->referral_type) && $referral->referral_type == 'In' ? 'selected' : null }}>
+                                        Referral In</option>
+                                    <option value="Out"
+                                        {{ isset($referral->referral_type) && $referral->referral_type == 'Out' ? 'selected' : null }}>
+                                        Referral Out</option>
                                 </select>
                                 @error('referral_type')
                                     <small class="text-danger fw-light">{{ $message }}</small>
@@ -72,9 +82,9 @@
                                 <select name="partner_id" class="select w-100" {{ $disabled }}>
                                     <option data-placeholder="true"></option>
                                     @foreach ($partners as $partner)
-                                        <option value="{{ $partner->corp_id }}" 
-                                                {{ isset($referral->partner_id) && $referral->partner_id == $partner->corp_id ? "selected" : null }}
-                                            >{{ $partner->corp_name }}</option>
+                                        <option value="{{ $partner->corp_id }}"
+                                            {{ isset($referral->partner_id) && $referral->partner_id == $partner->corp_id ? 'selected' : null }}>
+                                            {{ $partner->corp_name }}</option>
                                     @endforeach
                                 </select>
                                 @error('partner_id')
@@ -88,8 +98,10 @@
                                         <option data-placeholder="true"></option>
                                         @foreach ($programs as $program)
                                             <option value="{{ $program->prog_id }}"
-                                                    {{ isset($referral->prog_id) && $referral->prog_id == $program->prog_id ? "selected" : null }}
-                                                >{{ $program->prog_program }} from {{ $program->main_prog->prog_name }} {{ isset($program->sub_prog) ? ' - '.$program->sub_prog->sub_prog_name : null }}</option>
+                                                {{ isset($referral->prog_id) && $referral->prog_id == $program->prog_id ? 'selected' : null }}>
+                                                {{ $program->prog_program }} from {{ $program->main_prog->prog_name }}
+                                                {{ isset($program->sub_prog) ? ' - ' . $program->sub_prog->sub_prog_name : null }}
+                                            </option>
                                         @endforeach
                                     </select>
 
@@ -98,7 +110,8 @@
                                     @enderror
                                 </div>
                                 <div id="inputProgram" class="d-none">
-                                    <input type="text" name="additional_prog_name" value="{{ isset($referral->additional_prog_name) ? $referral->additional_prog_name : null }}"
+                                    <input type="text" name="additional_prog_name"
+                                        value="{{ isset($referral->additional_prog_name) ? $referral->additional_prog_name : null }}"
                                         class="form-control form-control-sm rounded" {{ $disabled }}>
                                 </div>
                                 @error('additional_prog_name')
@@ -107,7 +120,8 @@
                             </div>
                             <div class="col-md-6 mb-2">
                                 <label>Referral Date</label>
-                                <input type="date" name="ref_date" value="{{ isset($referral->ref_date) ? $referral->ref_date : null }}"
+                                <input type="date" name="ref_date"
+                                    value="{{ isset($referral->ref_date) ? $referral->ref_date : null }}"
                                     class="form-control form-control-sm rounded" {{ $disabled }}>
                                 @error('ref_date')
                                     <small class="text-danger fw-light">{{ $message }}</small>
@@ -115,7 +129,8 @@
                             </div>
                             <div class="col-md-6 mb-2">
                                 <label>Participant</label>
-                                <input type="number" name="number_of_student" value="{{ isset($referral->number_of_student) ? $referral->number_of_student : null }}"
+                                <input type="number" name="number_of_student"
+                                    value="{{ isset($referral->number_of_student) ? $referral->number_of_student : null }}"
                                     class="form-control form-control-sm rounded" {{ $disabled }}>
                                 @error('number_of_student')
                                     <small class="text-danger fw-light">{{ $message }}</small>
@@ -123,11 +138,23 @@
                             </div>
                             <div class="col-md-6 mb-2">
                                 <label>Amount</label>
-                                <input type="number" name="revenue" value="{{ isset($referral->revenue) ? $referral->revenue : null }}"
-                                    class="form-control form-control-sm rounded" {{ $disabled }}>
-                                @error('revenue')
-                                    <small class="text-danger fw-light">{{ $message }}</small>
-                                @enderror
+                                <div class="row g-0">
+                                    <div class="col-3">
+                                        <select name="" id="" class="select w-100">
+                                            <option value="IDR">IDR</option>
+                                            <option value="USD">USD</option>
+                                            <option value="SGD">SGD</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-9">
+                                        <input type="number" name="revenue"
+                                            value="{{ isset($referral->revenue) ? $referral->revenue : null }}"
+                                            class="form-control form-control-sm rounded" {{ $disabled }}>
+                                        @error('revenue')
+                                            <small class="text-danger fw-light">{{ $message }}</small>
+                                        @enderror
+                                    </div>
+                                </div>
                             </div>
                             <div class="col-md-12 mb-2">
                                 <label>Notes</label>
@@ -144,8 +171,8 @@
                                     <option data-placeholder="true"></option>
                                     @foreach ($employees as $employee)
                                         <option value="{{ $employee->id }}"
-                                                {{ isset($referral->empl_id) && $referral->empl_id == $employee->id ? "selected" : null }}
-                                            >{{ $employee->first_name.' '.$employee->last_name }}</option>
+                                            {{ isset($referral->empl_id) && $referral->empl_id == $employee->id ? 'selected' : null }}>
+                                            {{ $employee->first_name . ' ' . $employee->last_name }}</option>
                                     @endforeach
                                 </select>
                                 @error('empl_id')
@@ -180,7 +207,7 @@
 
         $(document).ready(function() {
 
-            @if (isset($referral) && $referral->referral_type == "In")
+            @if (isset($referral) && $referral->referral_type == 'In')
                 $('#selectProgram').removeClass('d-none')
                 $('#inputProgram').addClass('d-none')
             @else
@@ -188,11 +215,11 @@
                 $('#selectProgram').addClass('d-none')
             @endif
 
-        //     $("input[name=revenue]").change(function() {
-        //         var val = $(this).val()
+            //     $("input[name=revenue]").change(function() {
+            //         var val = $(this).val()
 
-        //         $(this).val(formatRupiah(val))
-        //     })
+            //         $(this).val(formatRupiah(val))
+            //     })
         })
     </script>
 @endsection
