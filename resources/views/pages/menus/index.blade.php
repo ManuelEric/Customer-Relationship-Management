daf5ce0f6cc846d5190ff10e343b26d3787fee55
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-03-01 11:33:43 +0700

    01/03/2023

--- resources/views/pages/menus/index.blade.php
+++ resources/views/pages/menus/index.blade.php
@@ -22,9 +22,11 @@
             <div id="user_card" class="card shadow mb-3 d-none">
                 <div class="card-header">Users</div>
                 <div class="card-body overflow-auto" style="max-height: 300px">
+                    <input type="text" name="" id="user_id">
                     <ul class="list-group list-group-flush">
                         @for ($i = 0; $i < 20; $i++)
-                            <li class="list-group-item d-flex justify-content-between cursor-pointer" onclick="checkUser('user_{{$i}}')">
+                            <li class="list-group-item d-flex justify-content-between cursor-pointer"
+                                onclick="checkUser('user_{{ $i }}')">
                                 User {{ $i }}
                                 <i class="bi bi-arrow-right"></i>
                             </li>
@@ -51,7 +53,8 @@
                                     <td width="80%">
                                         <div class="d-flex align-items-center gap-2">
                                             <input type="checkbox" name="menus" id="menus_{{ $i }}"
-                                                value="menus_{{ $i }}">
+                                                value="menus_{{ $i }}"
+                                                onchange="checkMenus('{{ $i }}')">
                                             <label for="menus_{{ $i }}" class="cursor-pointer">
                                                 Menus {{ $i }}
                                             </label>
@@ -59,11 +62,12 @@
                                     </td>
                                     <td width="10%" align="center">
                                         <input type="checkbox" name="copy" id="copy_{{ $i }}"
-                                            value="copy_{{ $i }}">
+                                            value="copy_{{ $i }}" onchange="checkCopy('{{ $i }}')">
                                     </td>
                                     <td width="10%" align="center">
                                         <input type="checkbox" name="export" id="export_{{ $i }}"
-                                            value="export_{{ $i }}">
+                                            value="export_{{ $i }}"
+                                            onchange="checkExport('{{ $i }}')">
                                     </td>
                                 </tr>
                             @endfor
@@ -78,7 +82,7 @@
         function checkDepartment() {
             let department = $('#department').val()
 
-            if(department!='') {
+            if (department != '') {
                 $('#user_card').removeClass('d-none')
             } else {
                 $('#user_card').addClass('d-none')
@@ -88,8 +92,54 @@
 
         function checkUser(user_id) {
             let department = $('#department').val()
+            $('#user_id').val(user_id)
             alert(user_id)
             //   Axios Here 
+        }
+
+        function checkMenus(menus_id) {
+            let department = $('#department').val()
+            let user = $('#user_id').val()
+            let menus = $('#menus_' + menus_id)
+
+            let menu_data
+            if (menus.is(':checked')) {
+                menu_data = true
+            } else {
+                menu_data = false
+            }
+
+            alert(menu_data)
+        }
+
+        function checkCopy(menus_id) {
+            let department = $('#department').val()
+            let user = $('#user_id').val()
+            let menus = $('#copy_' + menus_id)
+
+            let menu_data
+            if (menus.is(':checked')) {
+                menu_data = true
+            } else {
+                menu_data = false
+            }
+
+            alert(menu_data)
+        }
+
+        function checkExport(menus_id) {
+            let department = $('#department').val()
+            let user = $('#user_id').val()
+            let menus = $('#export_' + menus_id)
+            
+            let menu_data
+            if (menus.is(':checked')) {
+                menu_data = true
+            } else {
+                menu_data = false
+            }
+
+            alert(menu_data)
         }
     </script>
 @endsection
