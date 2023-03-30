14717e068cf0ba1304da0f38c467060e8dcff774
ahmadfauzan <fauzana566@gmail.com>
2023-03-30 10:06:55 +0700

    Add import excel v2

--- app/Exports/ClientEventTemplate.php
+++ app/Exports/ClientEventTemplate.php
@@ -18,26 +18,21 @@ use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 use PhpOffice\PhpSpreadsheet\Cell\DataValidation;
 use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class ClientEventTemplate implements FromCollection, WithHeadings, WithEvents, WithStrictNullComparison, WithColumnFormatting
+class ClientEventTemplate implements WithHeadings, WithEvents, WithStrictNullComparison, WithColumnFormatting
 {
     /**
      * @return \Illuminate\Support\Collection
      */
-    public function collection()
-    {
-        //
-        return UserClient::select(DB::raw('CONCAT(first_name, " ", COALESCE(last_name)) as full_name'))->limit(1)->get();
-    }
 
     public function headings(): array
     {
         $columns = [
             'Client Name',
-            'Event Name',
+            'Audience',
+            'Email',
+            'Phone Number',
+            'School Name',
             'Conversion Lead',
-            'Partner Name',
-            'Edufair Name',
-            'KOL Name',
             'Joined Date',
             'Status',
         ];
@@ -53,118 +48,12 @@ class ClientEventTemplate implements FromCollection, WithHeadings, WithEvents, W
 
                 // get layout counts (add 1 to rows for heading row)
                 $row_count = 2;
-                $column_count = 9;
+                $column_count = 8;
 
                 // set dropdown options
-                $fullname_options = UserClient::get()->pluck('full_name')->toArray();
-                $event_options = Event::get()->pluck('event_title')->toArray();
-                $lead_options = Lead::where('main_lead', '!=', 'KOL')->get()->pluck('main_lead')->toArray();
-                $partner_options = Corporate::get()->pluck('corp_name')->toArray();
-                $exteduf_options = EdufLead::leftJoin('tbl_corp', 'tbl_corp.corp_id', '=', 'tbl_eduf_lead.corp_id')
-                    ->leftJoin('tbl_sch', 'tbl_sch.sch_id', '=', 'tbl_eduf_lead.sch_id')
-                    ->select([
-                        DB::raw("(CASE WHEN tbl_eduf_lead.title = null OR tbl_eduf_lead.title = '' THEN
-                                        (CASE WHEN tbl_eduf_lead.corp_id is not null THEN
-                                            CONCAT(tbl_corp.corp_name, ' (', tbl_eduf_lead.event_start, ')')
-                                        ELSE
-                                            CONCAT(tbl_sch.sch_name, ' (', tbl_eduf_lead.event_start, ')')
-                                        END)
-                                ELSE
-                                        tbl_eduf_lead.title
-                                END) as organizer_name"),
-                    ])
-                    ->orderBy('organizer_name', 'asc')->get()->pluck('organizer_name')
-                    ->toArray();
-                $kol_options = Lead::where('main_lead', 'KOL')->orderBy('sub_lead', 'asc')->get()->pluck('sub_lead')->toArray();
                 $status_options = [
                     'Join', 'Attend'
                 ];
-
-                array_push($lead_options, 'KOL');
-
-                // set dropdown list for first data row
-                $validation = $event->sheet->getCell("A2")->getDataValidation();
-                $validation->setType(DataValidation::TYPE_LIST);
-                $validation->setErrorStyle(DataValidation::STYLE_INFORMATION);
-                $validation->setAllowBlank(false);
-                $validation->setShowInputMessage(true);
-                $validation->setShowErrorMessage(true);
-                $validation->setShowDropDown(true);
-                $validation->setErrorTitle('Input error');
-                $validation->setError('Value is not in list.');
-                $validation->setPromptTitle('Pick from list');
-                $validation->setPrompt('Please pick a value from the drop-down list.');
-                $validation->setFormula1(sprintf('"%s"', implode(',', $fullname_options)));
-
-                // set dropdown list for first data row
-                $validation = $event->sheet->getCell("B2")->getDataValidation();
-                $validation->setType(DataValidation::TYPE_LIST);
-                $validation->setErrorStyle(DataValidation::STYLE_INFORMATION);
-                $validation->setAllowBlank(false);
-                $validation->setShowInputMessage(true);
-                $validation->setShowErrorMessage(true);
-                $validation->setShowDropDown(true);
-                $validation->setErrorTitle('Input error');
-                $validation->setError('Value is not in list.');
-                $validation->setPromptTitle('Pick from list');
-                $validation->setPrompt('Please pick a value from the drop-down list.');
-                $validation->setFormula1(sprintf('"%s"', implode(',', $event_options)));
-
-                // set dropdown list for first data row
-                $validation = $event->sheet->getCell("C2")->getDataValidation();
-                $validation->setType(DataValidation::TYPE_LIST);
-                $validation->setErrorStyle(DataValidation::STYLE_INFORMATION);
-                $validation->setAllowBlank(false);
-                $validation->setShowInputMessage(true);
-                $validation->setShowErrorMessage(true);
-                $validation->setShowDropDown(true);
-                $validation->setErrorTitle('Input error');
-                $validation->setError('Value is not in list.');
-                $validation->setPromptTitle('Pick from list');
-                $validation->setPrompt('Please pick a value from the drop-down list.');
-                $validation->setFormula1(sprintf('"%s"', implode(',', $lead_options)));
-
-                // set dropdown list for first data row
-                $validation = $event->sheet->getCell("D2")->getDataValidation();
-                $validation->setType(DataValidation::TYPE_LIST);
-                $validation->setErrorStyle(DataValidation::STYLE_INFORMATION);
-                $validation->setAllowBlank(false);
-                $validation->setShowInputMessage(true);
-                $validation->setShowErrorMessage(true);
-                $validation->setShowDropDown(true);
-                $validation->setErrorTitle('Input error');
-                $validation->setError('Value is not in list.');
-                $validation->setPromptTitle('Pick from list');
-                $validation->setPrompt('Please pick a value from the drop-down list.');
-                $validation->setFormula1(sprintf('"%s"', implode(',', $partner_options)));
-
-                // set dropdown list for first data row
-                $validation = $event->sheet->getCell("E2")->getDataValidation();
-                $validation->setType(DataValidation::TYPE_LIST);
-                $validation->setErrorStyle(DataValidation::STYLE_INFORMATION);
-                $validation->setAllowBlank(false);
-                $validation->setShowInputMessage(true);
-                $validation->setShowErrorMessage(true);
-                $validation->setShowDropDown(true);
-                $validation->setErrorTitle('Input error');
-                $validation->setError('Value is not in list.');
-                $validation->setPromptTitle('Pick from list');
-                $validation->setPrompt('Please pick a value from the drop-down list.');
-                $validation->setFormula1(sprintf('"%s"', implode(',', $exteduf_options)));
-
-                // set dropdown list for first data row
-                $validation = $event->sheet->getCell("F2")->getDataValidation();
-                $validation->setType(DataValidation::TYPE_LIST);
-                $validation->setErrorStyle(DataValidation::STYLE_INFORMATION);
-                $validation->setAllowBlank(false);
-                $validation->setShowInputMessage(true);
-                $validation->setShowErrorMessage(true);
-                $validation->setShowDropDown(true);
-                $validation->setErrorTitle('Input error');
-                $validation->setError('Value is not in list.');
-                $validation->setPromptTitle('Pick from list');
-                $validation->setPrompt('Please pick a value from the drop-down list.');
-                $validation->setFormula1(sprintf('"%s"', implode(',', $kol_options)));
 
                 // set dropdown list for first data row
                 $validation = $event->sheet->getCell("H2")->getDataValidation();
@@ -193,7 +82,7 @@ class ClientEventTemplate implements FromCollection, WithHeadings, WithEvents, W
     public function columnFormats(): array
     {
         return [
-            'D' => NumberFormat::FORMAT_DATE_YYYYMMDD,
+            'G' => NumberFormat::FORMAT_DATE_YYYYMMDD,
         ];
     }
 }
--- app/Imports/ClientEventImport.php
+++ app/Imports/ClientEventImport.php
@@ -7,7 +7,10 @@ use App\Models\Corporate;
 use App\Models\EdufLead;
 use App\Models\Event;
 use App\Models\Lead;
+use App\Models\Role;
+use App\Models\School;
 use App\Models\UserClient;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
@@ -19,7 +22,7 @@ use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithValidation;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 
-class ClientEventImport implements ToCollection, WithHeadingRow, WithValidation
+class ClientEventImport implements ToCollection, WithHeadingRow
 {
     /**
      * @param Collection $collection
@@ -34,21 +37,71 @@ class ClientEventImport implements ToCollection, WithHeadingRow, WithValidation
         DB::beginTransaction();
         try {
             foreach ($rows as $row) {
+                $lastname = null;
+                $firstname = null;
+                $clientName = explode(' ', $row['client_name']);
+                // if (count($clientName) > 1) {
+                //     foreach ($clientName as $name => $key) {
+
+                //         // $firstname = $clientName[$name];
+                //         $name != count($clientName) - 1 ? $firstname = $clientName[$name] . ' ' : $clientName[$name];
+                //     }
+                //     $lastname = $clientName[count($clientName) - 1];
+                // } else {
+                //     $firstname = $row['client_name'];
+                // }
+
+                for ($i = 0; $i < count($clientName); $i++) {
+
+                    # looping at least two times
+                    if ($i <= 1)
+                        $isExistClient = UserClient::whereRaw("CONCAT(first_name, ' ', COALESCE(last_name, '')) like ?", ['%' . $clientName[$i] . '%'])->first();
+                }
+
+                if (!$isExistClient) {
+                    $clientDetails = [
+                        'first_name' => $row['client_name'],
+                        'last_name' => null,
+                        'mail' => $row['email'],
+                        'phone' => $row['phone_number'],
+                    ];
+
+                    $roleId = Role::whereRaw('LOWER(role_name) = (?)', [strtolower('student')])->first();
+                    $client = UserClient::create($clientDetails);
+                    $client->roles()->attach($roleId, [
+                        'created_at' => Carbon::now(),
+                        'updated_at' => Carbon::now()
+                    ]);
+                }
+
+                switch ($row['status']) {
+                    case 'Join':
+                        $status = 0;
+                        break;
+                    case 'Attend':
+                        $status = 1;
+                        break;
+
+                    case '':
+                        $status = null;
+                        break;
+
+                    default:
+                        $status = $row['status'];
+                        break;
+                }
+
+                $row['conversion_lead'] == 'School' ? $main_lead = 'School/Counselor' : $main_lead = $row['conversion_lead'];
+                $lead = Lead::select('lead_id')->where('main_lead', $main_lead)->first();
 
                 $data = [
-                    'client_id' => $row['client_name'],
-                    'event_id' => $row['event_name'],
-                    'partner_id' => $row['partner_name'],
-                    'lead_id' => $row['conversion_lead'],
-                    'eduf_id' => $row['edufair_name'],
-                    'joined_date' => $row['joined_date'],
-                    'status' => $row['status'],
+                    'client_id' => !$isExistClient ? $client->id : $isExistClient->id,
+                    'event_id' => 'EVT-0001',
+                    'lead_id' => isset($lead) ? $lead->lead_id : 'LS001',
+                    'joined_date' => isset($row['joined_date']) ? Date::excelToDateTimeObject($row['joined_date'])
+                        ->format('Y-m-d') : null,
+                    'status' => $status,
                 ];
-
-                if ($row['conversion_lead'] == 'KOL') {
-                    $data['lead_id'] = $row['kol_name'];
-                } else {
-                }
 
                 ClientEvent::insert($data);
             }
@@ -60,104 +113,110 @@ class ClientEventImport implements ToCollection, WithHeadingRow, WithValidation
         }
     }
 
-    public function prepareForValidation($data)
+    private function createSchoolIfNotExists($schName)
     {
-
-        DB::beginTransaction();
-        try {
-            $client = UserClient::select('id')->where(DB::raw('CONCAT(first_name, " ", COALESCE(last_name, ""))'), $data['client_name'])->first();
-            $event = Event::select('event_id')->where('event_title', $data['event_name'])->first();
-            $lead = null;
-            if ($data['conversion_lead'] == 'KOL') {
-                $kol = isset($data['kol_name']) ? Lead::select('lead_id')->where('sub_lead', $data['kol_name'])->first() : null;
-            } else {
-                $lead = Lead::select('lead_id')->where('main_lead', $data['conversion_lead'])->first();
-            }
-            $partner = Corporate::select('corp_id')->where('corp_name', $data['partner_name'])->first();
-            $exteduf = EdufLead::leftJoin('tbl_corp', 'tbl_corp.corp_id', '=', 'tbl_eduf_lead.corp_id')
-                ->leftJoin('tbl_sch', 'tbl_sch.sch_id', '=', 'tbl_eduf_lead.sch_id')
-                ->select('id')
-                ->where(DB::raw("(CASE WHEN tbl_eduf_lead.title COLLATE utf8mb4_unicode_ci = null OR tbl_eduf_lead.title COLLATE utf8mb4_unicode_ci = '' THEN
-                                        (CASE WHEN tbl_eduf_lead.corp_id COLLATE utf8mb4_unicode_ci is not null THEN
-                                            CONCAT(tbl_corp.corp_name COLLATE utf8mb4_unicode_ci, ' (', tbl_eduf_lead.event_start, ')')
-                                        ELSE
-                                            CONCAT(tbl_sch.sch_name COLLATE utf8mb4_unicode_ci, ' (', tbl_eduf_lead.event_start, ')')
-                                        END)
-                                ELSE
-                                        tbl_eduf_lead.title COLLATE utf8mb4_unicode_ci
-                                END)"), $data['edufair_name'])
-                ->first();
-
-            DB::commit();
-        } catch (Exception $e) {
-
-            DB::rollBack();
-            Log::error('Import client event failed : ' . $e->getMessage());
-        }
-
-        switch ($data['status']) {
-            case 'Join':
-                $status = 0;
-                break;
-            case 'Attend':
-                $status = 1;
-                break;
-
-            case '':
-                $status = null;
-                break;
-
-            default:
-                $status = $data['status'];
-                break;
-        }
-
-        $data = [
-            'client_name' => isset($client) ? $client->id : $data['client_name'],
-            'event_name' => isset($event) ? $event->event_id : $data['event_name'],
-            'conversion_lead' => isset($lead) ? $lead->lead_id : $data['conversion_lead'],
-            'lead_id' => isset($lead) ? $lead->lead_id : $data['conversion_lead'],
-            'main_lead' =>  isset($lead) ? $lead->main_lead : $data['conversion_lead'],
-            'partner_name' => null,
-            'edufair_name' => null,
-            'kol_name' => null,
-            'joined_date' => isset($data['joined_date']) ? Date::excelToDateTimeObject($data['joined_date'])
-                ->format('Y-m-d') : null,
-            'status' => $status,
-        ];
-
-        if ($data['lead_id'] == 'LS015') {
-            $data['partner_name'] = isset($partner) ? $partner->corp_id : $data['partner_name'];
-        } else if ($data['lead_id'] == 'LS018') {
-            $data['edufair_name'] = isset($exteduf) ? $exteduf->id : $data['edufair_name'];
-        } else if ($data['lead_id'] == 'KOL') {
-            $data['kol_name'] = isset($kol) ? $kol->lead_id : $data['kol_name'];
-            $data['conversion_lead'] = isset($kol) ? $kol->lead_id : $data['kol_name'];
-        }
-
-        return $data;
+        $school = School::whereSchoolName($schName)->first();
     }
 
-    public function rules(): array
-    {
-        return [
-            '*.client_name' => ['required', 'exists:tbl_client,id'],
-            '*.event_name' => ['required', 'exists:tbl_events,event_id'],
-            '*.conversion_lead' => ['required', 'exists:tbl_lead,lead_id'],
-            '*.partner_name' => ['required_if:lead_id,LS015', 'nullable', 'exists:tbl_corp,corp_id'],
-            '*.edufair_name' => ['required_if:lead_id,LS018', 'nullable'],
-            '*.kol_name' => ['required_if:main_lead,KOL', 'nullable'],
-            '*.joined_date' => ['required', 'date'],
-            '*.status' => ['required', 'in:0,1'],
-        ];
-    }
+    // public function prepareForValidation($data)
+    // {
 
-    public function customValidationMessages()
-    {
-        return [
-            '*.partner_name.required_if' => 'The :attribute field is required when conversion lead is All-In Partners.',
-            '*.edufair_name.required_if' => 'The :attribute field is required when conversion lead is External Edufair.',
-            '*.kol_name.required_if' => 'The :attribute field is required when conversion lead is KOL.',
-        ];
-    }
+    //     DB::beginTransaction();
+    //     try {
+    //         $client = UserClient::select('id')->where(DB::raw('CONCAT(first_name, " ", COALESCE(last_name, ""))'), $data['client_name'])->first();
+    //         $event = Event::select('event_id')->where('event_title', $data['event_name'])->first();
+    //         $lead = null;
+    //         if ($data['conversion_lead'] == 'KOL') {
+    //             $kol = isset($data['kol_name']) ? Lead::select('lead_id')->where('sub_lead', $data['kol_name'])->first() : null;
+    //         } else {
+    //             $lead = Lead::select('lead_id')->where('main_lead', $data['conversion_lead'])->first();
+    //         }
+    //         $partner = Corporate::select('corp_id')->where('corp_name', $data['partner_name'])->first();
+    //         $exteduf = EdufLead::leftJoin('tbl_corp', 'tbl_corp.corp_id', '=', 'tbl_eduf_lead.corp_id')
+    //             ->leftJoin('tbl_sch', 'tbl_sch.sch_id', '=', 'tbl_eduf_lead.sch_id')
+    //             ->select('id')
+    //             ->where(DB::raw("(CASE WHEN tbl_eduf_lead.title COLLATE utf8mb4_unicode_ci = null OR tbl_eduf_lead.title COLLATE utf8mb4_unicode_ci = '' THEN
+    //                                     (CASE WHEN tbl_eduf_lead.corp_id COLLATE utf8mb4_unicode_ci is not null THEN
+    //                                         CONCAT(tbl_corp.corp_name COLLATE utf8mb4_unicode_ci, ' (', tbl_eduf_lead.event_start, ')')
+    //                                     ELSE
+    //                                         CONCAT(tbl_sch.sch_name COLLATE utf8mb4_unicode_ci, ' (', tbl_eduf_lead.event_start, ')')
+    //                                     END)
+    //                             ELSE
+    //                                     tbl_eduf_lead.title COLLATE utf8mb4_unicode_ci
+    //                             END)"), $data['edufair_name'])
+    //             ->first();
+
+    //         DB::commit();
+    //     } catch (Exception $e) {
+
+    //         DB::rollBack();
+    //         Log::error('Import client event failed : ' . $e->getMessage());
+    //     }
+
+    //     switch ($data['status']) {
+    //         case 'Join':
+    //             $status = 0;
+    //             break;
+    //         case 'Attend':
+    //             $status = 1;
+    //             break;
+
+    //         case '':
+    //             $status = null;
+    //             break;
+
+    //         default:
+    //             $status = $data['status'];
+    //             break;
+    //     }
+
+    //     $data = [
+    //         'client_name' => isset($client) ? $client->id : $data['client_name'],
+    //         'event_name' => isset($event) ? $event->event_id : $data['event_name'],
+    //         'conversion_lead' => isset($lead) ? $lead->lead_id : $data['conversion_lead'],
+    //         'lead_id' => isset($lead) ? $lead->lead_id : $data['conversion_lead'],
+    //         'main_lead' =>  isset($lead) ? $lead->main_lead : $data['conversion_lead'],
+    //         'partner_name' => null,
+    //         'edufair_name' => null,
+    //         'kol_name' => null,
+    //         'joined_date' => isset($data['joined_date']) ? Date::excelToDateTimeObject($data['joined_date'])
+    //             ->format('Y-m-d') : null,
+    //         'status' => $status,
+    //     ];
+
+    //     if ($data['lead_id'] == 'LS015') {
+    //         $data['partner_name'] = isset($partner) ? $partner->corp_id : $data['partner_name'];
+    //     } else if ($data['lead_id'] == 'LS018') {
+    //         $data['edufair_name'] = isset($exteduf) ? $exteduf->id : $data['edufair_name'];
+    //     } else if ($data['lead_id'] == 'KOL') {
+    //         $data['kol_name'] = isset($kol) ? $kol->lead_id : $data['kol_name'];
+    //         $data['conversion_lead'] = isset($kol) ? $kol->lead_id : $data['kol_name'];
+    //     }
+
+    //     return $data;
+    // }
+
+    // public function rules(): array
+    // {
+    //     return [
+    //         '*.client_name' => ['required'],
+    //         '*.client_name' => ['required'],
+    //         '*.event_name' => ['required'],
+    //         '*.conversion_lead' => ['required', 'exists:tbl_lead,lead_id'],
+    //         '*.partner_name' => ['required_if:lead_id,LS015', 'nullable', 'exists:tbl_corp,corp_id'],
+    //         '*.edufair_name' => ['required_if:lead_id,LS018', 'nullable'],
+    //         '*.kol_name' => ['required_if:main_lead,KOL', 'nullable'],
+    //         '*.joined_date' => ['required', 'date'],
+    //         '*.status' => ['required', 'in:0,1'],
+    //     ];
+    // }
+
+    // public function customValidationMessages()
+    // {
+    //     return [
+    //         '*.partner_name.required_if' => 'The :attribute field is required when conversion lead is All-In Partners.',
+    //         '*.edufair_name.required_if' => 'The :attribute field is required when conversion lead is External Edufair.',
+    //         '*.kol_name.required_if' => 'The :attribute field is required when conversion lead is KOL.',
+    //     ];
+    // }
 }
