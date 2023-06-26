0d7d2a5a94ddd0d51e650bce66ba3034871aa540
unknown <manuel.eric.b@gmail.com>
2023-06-26 09:17:33 +0700

    update embed form function

--- app/Http/Controllers/Embed/PublicRegistrationController.php
+++ app/Http/Controllers/Embed/PublicRegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Embed;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePublicRegistrationRequest;
+use App\Http\Traits\CheckExistingClient;
 use App\Http\Traits\CreateCustomPrimaryKeyTrait;
 use App\Http\Traits\StandardizePhoneNumberTrait;
 use App\Interfaces\ClientRepositoryInterface;
@@ -14,6 +15,7 @@ use Illuminate\Http\Request;
 class PublicRegistrationController extends Controller
 {
     use CreateCustomPrimaryKeyTrait;
+    use CheckExistingClient;
 
     private SchoolRepositoryInterface $schoolRepository;
     private ClientRepositoryInterface $clientRepository;
@@ -28,37 +30,71 @@ class PublicRegistrationController extends Controller
     {
         $schools = $this->schoolRepository->getAllSchools();
 
-        return view('form-embed.form-website')->with([
+        return view('form-embed.form-general')->with([
             'schools' => $schools
         ]);
     }
 
     public function store(StorePublicRegistrationRequest $request)
     {
-        $parent_or_children = $request->parent_or_children;
+        // return $request->all();
+        $role = $request->role;
+        
+        if (count(array_filter($request->fullname)) > 1) {
 
+            # there are parent and children 
+            $parentDetail = [
+                'fullname' => $request->fullname[0],
+                'mail' => $request->email[0],
+                'phone' => $request->fullnumber[0] 
+            ];
+
+            $childrenDetail = [
+                'fullname' => $request->fullname[1],
+                'mail' => $request->email[1],
+                'phone' => $request->fullnumber[1],
+                'school' => $request->school,
+                'grade' => $request->grade,
+                'program' => $request->program
+            ];
+
+        } else {
+
+            $childrenDetail = [
+                'fullname' => $request->fullname[0],
+                'mail' => $request->email[0],
+                'phone' => $request->fullnumber[0],
+                'school' => $request->school,
+                'grade' => $request->grade,
+                'program' => $request->program
+            ];
+        }        
+
+        $newParent = false;
         # checking if client was a parent
-        if ($parent_or_children == "parent")
-            $newParent = $this->storeParentIfNotExists($request);
+        if ($role == "parent")
+            $newParent = $this->storeParentIfNotExists($parentDetail);
         
         # checking if client was a child
-        $newChild = $this->storeChildrenIfNotExists($request);
+        $newChild = $this->storeChildrenIfNotExists($childrenDetail);
         
 
         # create relation between parent & student
         if ($newParent && $newChild) 
             $this->clientRepository->createClientRelation($newParent['id'], $newChild['id']);
 
+        return 'Registration success';
+
     }
 
-    private function storeParentIfNotExists(Request $request) 
+    private function storeParentIfNotExists($detail) 
     {
-        $first_name = $request->parent_name;
+        $first_name = $detail['fullname'];
         $last_name = null; # set null as default because the embedded registration form only shows full names which there's no first_name and last_name
 
         # to retrieve first_name and last_name
         # check parent_name if there are multiple words
-        $explode = explode(" ", $request->parent_name);
+        $explode = explode(" ", $detail['fullname']);
         if (count($explode) > 1 ) {
             $first_name = $explode[0];
             $last_name = $explode(max($explode));
@@ -68,41 +104,46 @@ class PublicRegistrationController extends Controller
         $parentDetail = [
             'first_name' => $first_name,
             'last_name' => $last_name,
-            'mail' => $request->parent_email,
-            'phone' => $request->parent_phone,
+            'mail' => $detail['mail'],
+            'phone' => $detail['phone'],
         ];
     
         # check if parent mail & phone exists
-        if ($existingParent = $this->checkExistingClient($parentDetail['phone'], $parentDetail['mail'])) 
-            return $existingParent['id'];
+        if ($existingParent = $this->checkExistingClient($parentDetail['phone'], $parentDetail['mail'])) {
+            if (isset($existingChild['id']))
+                return $existingChild['id'];
+        }
 
         # when not exist then store it
         return $existingParent['id'] = $this->clientRepository->createClient('Parent', $parentDetail)->id;
     }
 
-    private function storeChildrenIfNotExists(Request $request)
+    private function storeChildrenIfNotExists($detail)
     {
-        $explode = explode(" ", $request->child_name);
+        $first_name = $detail['fullname'];
+        $last_name = null; # set null as default because the embedded registration form only shows full names which there's no first_name and last_name
+
+        $explode = explode(" ", $detail['fullname']);
         if (count($explode) > 1 ) {
             $first_name = $explode[0];
             $last_name = $explode(max($explode));
         } 
 
-        $grade = ($request->grade) > date('Y') ? $request->grade - date('Y') : 13;
+        $grade = ($detail['grade']) > date('Y') ? $detail['grade'] - date('Y') : 13;
 
         $studentDetail = [
             'first_name' => $first_name,
             'last_name' => $last_name,
-            'mail' => $request->child_email,
-            'phone' => $request->child_phone,
-            'school' => $request->child_school,
-            'graduation_year' => $request->grade,
+            'mail' => $detail['mail'],
+            'phone' => $detail['phone'],
+            'school' => $detail['school'],
+            'graduation_year' => $detail['grade'],
             'st_grade' => $grade,
-            'preferred_program' => $request->program,
+            'preferred_program' => $detail['program'],
 
         ];
 
-        if ($studentDetail['school'] == "new-school") {
+        if (!$this->schoolRepository->getSchoolById($studentDetail['school'])) {
 
             $last_id = School::max('sch_id');
             $school_id_without_label = $last_id ? $this->remove_primarykey_label($last_id, 4) : '0000';
@@ -110,7 +151,7 @@ class PublicRegistrationController extends Controller
 
             $schoolDetail = [
                 'sch_id' => $school_id_with_label,
-                'sch_name' => $request->new_school_box
+                'sch_name' => $detail['school']
             ];
 
             # store school
@@ -119,8 +160,10 @@ class PublicRegistrationController extends Controller
         }
 
         # check if student mail & phone exists
-        if ($existingChild = $this->checkExistingClient($studentDetail['phone'], $studentDetail['mail'])) 
-            return $existingChild['id'];
+        if ($existingChild = $this->checkExistingClient($studentDetail['phone'], $studentDetail['mail'])) {
+            if (isset($existingChild['id']))
+                return $existingChild['id'];
+        }
 
         # when not exist then store it
         return $existingChild['id'] = $this->clientRepository->createClient('Student', $studentDetail)->id;
--- app/Http/Requests/StorePublicRegistrationRequest.php
+++ app/Http/Requests/StorePublicRegistrationRequest.php
@@ -24,16 +24,7 @@ class StorePublicRegistrationRequest extends FormRequest
     public function rules()
     {
         return [
-            'parent_or_children' => 'required|in:parent,children',
-            'parent_name' => 'required_if:parent_or_children,parent|string',
-            'parent_email' => 'required_if:parent_or_children,parent|email',
-            'parent_phone' => 'required_if:parent_or_children,parent',
-            'child_name' => 'required_if:parent_or_children,children|string',
-            'chilld_email' => 'required_if:parent_or_children,children|email',
-            'chilld_phone' => 'required_if:parent_or_children,children',
-            'chilld_school' => 'required_if:parent_or_children,children',
-            'chilld_grade' => 'required_if:parent_or_children,children',
-            'chilld_program' => 'required_if:parent_or_children,children'
+            // 'role' => 'required|in:parent,children',
         ];
     }
 }
