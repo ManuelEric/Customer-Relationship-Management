a8a64ee7cf1f4c967f8e07405d3fd66ec77720a6
ahmadfauzan <fauzana566@gmail.com>
2023-07-04 13:42:17 +0700

    add button reminder whatsapp to dashboard

--- app/Repositories/InvoiceProgramRepository.php
+++ app/Repositories/InvoiceProgramRepository.php
@@ -327,14 +327,17 @@ class InvoiceProgramRepository implements InvoiceProgramRepositoryInterface
             ->leftJoin('program', 'program.prog_id', '=', 'tbl_client_prog.prog_id')
             // ->leftJoin('tbl_main_prog', 'tbl_main_prog.id', '=', 'tbl_prog.main_prog_id')
             // ->leftJoin('tbl_sub_prog', 'tbl_sub_prog.id', '=', 'tbl_prog.sub_prog_id')
-            ->leftJoin('tbl_client', 'tbl_client.id', '=', 'tbl_client_prog.client_id');
+            ->leftJoin('tbl_client as child', 'child.id', '=', 'tbl_client_prog.client_id')
+            ->leftJoin('tbl_client_relation', 'tbl_client_relation.child_id', '=', 'child.id')
+            ->leftJoin('tbl_client as parent', 'parent.id', '=', 'tbl_client_relation.parent_id');
 
         switch ($type) {
             case 'paid':
                 $queryInv->select([
                     'tbl_inv.inv_id as invoice_id',
                     'tbl_inv.clientprog_id',
-                    DB::raw('CONCAT(first_name, " ", COALESCE(last_name, "")) as full_name'),
+                    DB::raw('CONCAT(child.first_name, " ", COALESCE(child.last_name, "")) as full_name'),
+                    'parent.phone as parent_phone',
                     'program.program_name',
                     // DB::raw('CONCAT(prog_program, " - ", COALESCE(tbl_main_prog.prog_name, ""), COALESCE(CONCAT(" / ", tbl_sub_prog.sub_prog_name), "")) as program_name'),
                     'tbl_inv.inv_totalprice_idr as total_price_inv',
@@ -356,7 +359,9 @@ class InvoiceProgramRepository implements InvoiceProgramRepositoryInterface
                     'tbl_inv.inv_id as invoice_id',
                     'tbl_inv.clientprog_id',
                     'tbl_inv.clientprog_id as client_prog_id',
-                    DB::raw('CONCAT(first_name, " ", COALESCE(last_name, "")) as full_name'),
+                    'child.id as client_id',
+                    DB::raw('CONCAT(child.first_name, " ", COALESCE(child.last_name, "")) as full_name'),
+                    'parent.phone as parent_phone',
                     'program.program_name',
                     // DB::raw('CONCAT(prog_program, " - ", COALESCE(tbl_main_prog.prog_name, ""), COALESCE(CONCAT(" / ", tbl_sub_prog.sub_prog_name), "")) as program_name'),
                     'tbl_invdtl.invdtl_installment as installment_name',
