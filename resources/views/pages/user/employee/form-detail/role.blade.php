d4d29339e7c57aa6573705c35c0a5460558ac311
ahmadfauzan <fauzana566@gmail.com>
2023-07-13 09:59:22 +0700

    Revise editor & hide button add editor

--- resources/views/pages/user/employee/form-detail/role.blade.php
+++ resources/views/pages/user/employee/form-detail/role.blade.php
@@ -73,7 +73,7 @@
                     <option data-placeholder="true"></option>
                     @foreach ($positions as $position)
                         <option value="{{ $position->id }}" 
-                            @selected(isset($user) && $user->position->id == $position->id) 
+                            @selected(isset($user) && isset($user->position) && $user->position->id == $position->id) 
                             @selected(old('position') == $position->id)>{{ $position->position_name }}</option>
                     @endforeach
                 </select>
