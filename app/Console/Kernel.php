449a822facf5e4792a858e89f9cda5ad1fa885a5
unknown <manuel.eric.b@gmail.com>
2023-07-28 09:34:34 +0700

    comment scheduler reminder HR

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -72,11 +72,11 @@ class Kernel extends ConsoleKernel
         $schedule->command('send:reminder_invoicereferral_program')->everyFiveMinutes();
 
         $schedule->command('send:reminder_followup')->daily(); # daily needed!
-        $schedule->command('send:reminder_expiration_contracts_probation')->daily(); # daily needed!
-        $schedule->command('send:reminder_expiration_contracts_tutor')->daily(); # daily needed!
-        $schedule->command('send:reminder_expiration_contracts_editor')->daily(); # daily needed!
-        $schedule->command('send:reminder_expiration_contracts_external_mentor')->daily(); # daily needed!
-        $schedule->command('send:reminder_expiration_contracts_internship')->daily(); # daily needed!
+        // $schedule->command('send:reminder_expiration_contracts_probation')->daily(); # daily needed!
+        // $schedule->command('send:reminder_expiration_contracts_tutor')->daily(); # daily needed!
+        // $schedule->command('send:reminder_expiration_contracts_editor')->daily(); # daily needed!
+        // $schedule->command('send:reminder_expiration_contracts_external_mentor')->daily(); # daily needed!
+        // $schedule->command('send:reminder_expiration_contracts_internship')->daily(); # daily needed!
         // $schedule->command('send:reminder_tutor_h1')->daily();
         // $schedule->command('send:reminder_tutor_t3')->daily();
     }
