e6a167439cd09c2a4c54bd493310436eeb1f313c
ahmadfauzan <fauzana566@gmail.com>
2023-08-04 15:30:10 +0700

    Revise detail mentee

--- app/Http/Controllers/ClientMenteeController.php
+++ app/Http/Controllers/ClientMenteeController.php
@@ -42,10 +42,12 @@ class ClientMenteeController extends Controller
     {
         $menteeId = $request->route('mentee');
         $student = $this->clientRepository->getClientById($menteeId);
+        $viewStudent = $this->clientRepository->getViewClientById($studentId);
 
         return view('pages.client.student.view')->with(
             [
-                'student' => $student
+                'student' => $student,
+                'viewStudent' => $viewStudent
             ]
         );
     }
