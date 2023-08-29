be204662a249398659050c978ec3a8e7350fc798
unknown <manuel.eric.b@gmail.com>
2023-08-29 16:16:29 +0700

    fix:digital dashboard

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -69,54 +69,60 @@
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item dropdown d-none d-lg-block user-dropdown me-lg-3 me-0">
-                        <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="bi bi-bell"></i>
-                            @if (isset($countAlarm))
-                                @php
-                                    if($isSales){
-                                        $count = $countAlarm['sales'];
-                                    }elseif($isAdmin){
-                                        $count = $countAlarm['general'];
-                                    }
-                                @endphp
-                                @if($count > 0)
-                                    <span
-                                        class="position-absolute ms-1 top-1 start-100 translate-middle badge rounded-pill bg-danger"
-                                        style="font-size: 11px">
-                                        <small>
-                                            {{ $count }}
-                                        </small>
-                                    </span>
-                                @endif
-                            @endif
-                        </a>
-
-                        @if (isset($notification))
-                            <ul class="dropdown-menu dropdown-menu-right navbar-dropdown py-2 px-4" style="width: 400px;">
-                                @php
-                                    if($isSales){
-                                        $notification = $notification['sales'];
-                                    }elseif($isDigital){
-                                        $notification = $notification['digital'];
-                                    }elseif($isAdmin){
-                                        $notification = $notification['general'];
-                                    }
-                                @endphp
-                                @foreach ($notification as $notif)
-                                                    
-                                    <li class="d-flex align-items-center border-bottom py-2">
-                                        
-                                        <i class="bi bi-exclamation-circle me-2"></i>
-                                        <span class="lh-sm">
-                                            {!! $notif !!}
+                    
+                    @if ($isAdmin || $isSales || $isDigital)
+                        <li class="nav-item dropdown d-none d-lg-block user-dropdown me-lg-3 me-0">
+                            <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="bi bi-bell"></i>
+                                @if (isset($countAlarm))
+                                    @php
+                                        if($isSales){
+                                            $count = $countAlarm['sales'];
+                                        }elseif($isAdmin){
+                                            $count = $countAlarm['general'];
+                                        }elseif($isDigital) {
+                                            $count = $countAlarm['digital'];
+                                        }
+                                    @endphp
+                                    @if($count > 0)
+                                        <span
+                                            class="position-absolute ms-1 top-1 start-100 translate-middle badge rounded-pill bg-danger"
+                                            style="font-size: 11px">
+                                            <small>
+                                                {{ $count }}
+                                            </small>
                                         </span>
-                                    </li>
-                                @endforeach
+                                    @endif
+                                @endif
+                            </a>
 
-                            </ul>
-                        @endif
-                    </li>
+                            @if (isset($notification))
+                                <ul class="dropdown-menu dropdown-menu-right navbar-dropdown py-2 px-4" style="width: 400px;">
+                                    @php
+                                        if($isSales){
+                                            $notification = $notification['sales'];
+                                        }elseif($isDigital){
+                                            $notification = $notification['digital'];
+                                        }elseif($isAdmin){
+                                            $notification = $notification['general'];
+                                        }
+                                    @endphp
+                                    @foreach ($notification as $notif)
+                                                        
+                                        <li class="d-flex align-items-center border-bottom py-2">
+                                            
+                                            <i class="bi bi-exclamation-circle me-2"></i>
+                                            <span class="lh-sm">
+                                                {!! $notif !!}
+                                            </span>
+                                        </li>
+                                    @endforeach
+
+                                </ul>
+                            @endif
+                        </li>
+                        
+                    @endif
                     <li class="nav-item dropdown d-none d-lg-block user-dropdown">
                         <a class="nav-link" id="UserDropdown" href="#" data-bs-toggle="dropdown"
                             aria-expanded="false">
