ef7f868aa82482795619f9567a47e9a363d0e060
ahmadfauzan <fauzana566@gmail.com>
2023-09-18 14:55:28 +0700

    Add download qr

--- resources/views/scan-qrcode/qrcode.blade.php
+++ resources/views/scan-qrcode/qrcode.blade.php
@@ -10,8 +10,12 @@
         <div class="container-fluid mt-3">
             <div class="row align-items-center justify-content-center" style="height:95vh;">
                 
-                <div class="col-md-3">
-                    <div class="card shadow mb-3" style="background: #233469;">
+                <center>
+                    <h4>Thank you for joining the STEM+ Wonderlab,</h4>
+                    <h5>Here your QR Code</h5>
+                </center>
+                <div class="col-md-3" id="my-qr">
+                    <div class="card shadow mb-3 mt-2" style="background: #233469;">
                         <div class="card-body">
                             <div class="card bg-light">
                                 <div class="card-body text-center position-relative">
@@ -30,8 +34,10 @@
                     </div>
 
                     {{-- Instruction  --}}
+                    <div class="mb-2">
+
                     <div class="card shadow">
-                        <div class="card-header" style="background: #233469;">
+                        <div class="card-header mb-2" style="background: #233469;">
                             <h5 class="p-0 m-0 text-white  d-flex justify-content-between">
                                 Instructions
                                 <i class="bi bi-info-circle me-2"></i>
@@ -51,7 +57,29 @@
                         </div>
                     </div>
                 </div>
+
+
+                    
+                </div>
+                <center>
+                    <button id="dl-qr" class="btn btn-primary" style="background: #233469;">Download My QR</button>
+                </center>
             </div>
         </div>
+
+        <script>
+            $("#dl-qr").click(function(){
+                const screenshotTarget = document.getElementById('my-qr');
+
+                html2canvas(screenshotTarget).then((canvas) => {
+                    const base64image = canvas.toDataURL("image/png");
+                    var anchor = document.createElement('a');
+                    anchor.setAttribute("href", base64image);
+                    anchor.setAttribute("download", "my-qr.png");
+                    anchor.click();
+                    anchor.remove();
+                });
+            });
+        </script>
     </section>
 @endsection
