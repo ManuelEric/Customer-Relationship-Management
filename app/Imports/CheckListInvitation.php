47eb314c03692b11991721cc477703240d9bb2b6
ahmadfauzan <fauzana566@gmail.com>
2023-10-03 14:47:42 +0700

    Add function check list invitation

--- app/Imports/CheckListInvitation.php
+++ app/Imports/CheckListInvitation.php
@@ -19,6 +19,7 @@ use App\Models\Event;
 use App\Models\UserClient;
 use App\Models\UserClientAdditionalInfo;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\View;
 
 class CheckListInvitation implements ToCollection, WithHeadingRow, WithValidation
 {
@@ -41,15 +42,14 @@ class CheckListInvitation implements ToCollection, WithHeadingRow, WithValidatio
             $client = UserClient::where('mail', $row['parent_mail'])->first();
             $clientMail = UserClient::where('mail', $row['parent_mail'])->get();
 
-            if(isset($client)){
+            if(isset($client) && isset($row['parent_mail'])){
                 $childs = $client->childrens()->get();
     
-                $dataChild = [];
+                $dataChildDb = [];
+                $dataChildExcel = [];
                 foreach ($childs as $child) {
-                    $dataChild[] = [
-                        'full_name_db' => $child->full_name,
-                        'full_name_excel' => $row['name'],
-                    ];
+                    $dataChildDb[] = $child->full_name;
+                    $dataChildExcel[] = $row['name'];
                 }
                 $data[]=[
                     'client_id' => $client->id,
@@ -62,9 +62,8 @@ class CheckListInvitation implements ToCollection, WithHeadingRow, WithValidatio
                     'parent_phone_excel' => $row['parent_phone'],
                     'phoneIsEqual' => $client->phone == $row['parent_phone'] ? 'true' : 'false',
                     'parentNameIsEqual' => $client->full_name == $row['parent_name'] ? 'true' : 'false',
-                    'childs' => [
-                        $dataChild
-                    ]
+                    'childs_db' => implode(",", $dataChildDb),
+                    'childs_excel' => implode(",", $dataChildExcel),
                 ];
 
                 
@@ -80,7 +79,8 @@ class CheckListInvitation implements ToCollection, WithHeadingRow, WithValidatio
 
         }
 
-        Log::info($data);
+        // return View::make('mailing.stem-wonderlab', compact('data'));
+        Log::info(json_encode($data));
 
     }
           
@@ -104,7 +104,7 @@ class CheckListInvitation implements ToCollection, WithHeadingRow, WithValidatio
         return [
             '*.name' => ['nullable'],
             '*.parent_name' => ['nullable'],
-            '*.parent_mail' => ['required'],
+            '*.parent_mail' => ['nullable'],
             '*.parent_phone' => ['nullable'],
         ];
     }
