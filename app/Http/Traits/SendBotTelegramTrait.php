a35091db29ed2d25019dec998a5b005142030380
ahmadfauzan <fauzana566@gmail.com>
2023-10-12 11:37:19 +0700

    Update trait send bot telegram

--- app/Http/Traits/SendBotTelegramTrait.php
+++ app/Http/Traits/SendBotTelegramTrait.php
@@ -4,17 +4,23 @@ namespace App\Http\Traits;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Crypt;
 
-trait sendBotTelegramTrait {
+trait SendBotTelegramTrait {
 
-    public function sendMessageTele($tokenBot, $teleId, $message)
+    public function sendMessageTele($tokenBot, $teleId, $type, $message)
     {
         $client  = new Client();
         $url = "https://api.telegram.org/".$tokenBot."/sendMessage";
+
+        switch ($type) {
+            case 'log-error':
+                $link = url('log-viewer/logs/'.date('Y-m-d').'/error');
+                break;
+        }
         
         $client->request('GET', $url, [
             'json' =>[
             "chat_id" => $teleId, 
-            "text" => 'CRM_' . date('y/m/d') .' '. substr($message, 0, 100), # Limit message telegram
+            "text" => 'CRM_' . date('y/m/d') .' '. substr($message, 0, 100) . ' ' . $link, # Limit message telegram
             ]
         ]);
     }
