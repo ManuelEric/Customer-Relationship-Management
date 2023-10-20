59848c50e0bc065a2105e611c17db32664b08a80
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-10-20 16:45:49 +0700

    add student acceptance

--- resources/views/pages/client/student/index-mentee.blade.php
+++ resources/views/pages/client/student/index-mentee.blade.php
@@ -12,7 +12,7 @@
                 </h5>
             </div>
             <div class="col-md-6 d-flex justify-content-end">
-                <a href="{{url('alumni-acceptance')}}" class="btn btn-sm bg-light me-2">
+                <a href="{{url('client/alumni-acceptance')}}" class="btn btn-sm bg-light me-2">
                     Alumni Acceptance
                 </a>
                 <div class="dropdown">
