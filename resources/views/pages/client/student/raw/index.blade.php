84365839e466d9c3b39ecc26d8347c94dbcd8d32
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2023-12-07 16:39:06 +0700

    TKCRM-20 update layout

--- resources/views/pages/client/student/raw/index.blade.php
+++ resources/views/pages/client/student/raw/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.main')
 
-@section('title', 'Student')
+@section('title', 'Raw Data Student')
 
 @push('styles')
     <link rel="stylesheet" href="{{ asset('library/dashboard/css/vertical-layout-light/style.css') }}">
@@ -191,8 +191,9 @@
                     <thead class="bg-secondary text-white">
                         <tr class="text-center" role="row">
                             <th class="bg-info text-white">#</th>
+                            <th class="bg-info text-white">
+                                <i class="bi bi-check"></i>
                             </th>
-                            <th class="bg-info text-white">No</th>
                             <th class="bg-info text-white">Name</th>
                             <th class="bg-info text-white">Suggestion</th>
                             <th>Mail</th>
@@ -274,13 +275,28 @@
                         '</tr>';
                     clientSuggest.forEach(function(item, index) {
                         joined_program = '';
-                        if (item.client_program.length > 0) {
+                        program_limit = 2;
+                        program_number = item.client_program.length;
+                        program_other = program_number - program_limit;
+                        check_school = item.school?.is_verified == 'Y' ?
+                            '<i class="bi bi-check-circle-fill text-success"></i>' :
+                            '<i class="bi bi-x-circle-fill text-danger"></i>';
+
+                        if (program_number > 0) {
                             item.client_program.forEach(function(clientprog, index) {
-                                if (clientprog.status == 1) {
+                                if (clientprog.status == 1 && index < program_limit) {
                                     joined_program += clientprog.program.program_name;
-                                    (item.client_program.length !== index + 1 ? joined_program +=
+                                    (program_number !== index + 1 ? joined_program +=
                                         ', ' : '')
+
+                                    if (index == (program_limit - 1) && program_number >
+                                        program_limit) {
+                                        joined_program +=
+                                            '<div class="badge bg-warning text-dark py-1 px-2">' +
+                                            program_other + ' More </div>'
+                                    }
                                 }
+
                             })
                         }
 
@@ -289,13 +305,17 @@
                             '<td><input type="radio" name="similar' + d.id +
                             '" class="form-check-input item-' + item.id + '" onclick="comparison(' +
                             d.id + ',' + item.id + ')" /></td>' +
-                            '<td>' + item.first_name + ' ' + (item.last_name !== null ? item.last_name : '') + '</td>' +
+                            '<td><i class="bi bi-person"></i> ' + item.first_name + ' ' + (item
+                                .last_name !== null ? item.last_name :
+                                '') + '</td>' +
                             '<td>' + (item.mail !== null ? item.mail : '-') + '</td>' +
                             '<td>' + (item.phone !== null ? item.phone : '-') + '</td>' +
                             '<td>' + (typeof item.school !== 'undefined' && item.school !== null ? item
-                                .school.sch_name : '-') + '</td>' +
-                            '<td>' + (item.parents.length > 0 ? item.parents[0].first_name + ' ' + (item
-                                .parents[0].last_name !== null ? item.parents[0].last_name : '') : '-') +
+                                .school.sch_name + ' ' + check_school : '-') + '</td>' +
+                            '<td><i class="bi bi-person"></i> ' + (item.parents.length > 0 ? item.parents[0]
+                                .first_name + ' ' + (item
+                                    .parents[0].last_name !== null ? item.parents[0].last_name : '') : '-'
+                            ) +
                             '</td>' +
                             '<td>' + (item.graduation_year_real !== null ? item.graduation_year_real :
                                 '-') + '</td>' +
@@ -308,18 +328,7 @@
                     });
                 }
 
-                similar +=
-                    '<tr>' +
-                    '<th colspan=8>Convert without Comparison</th>' +
-                    '</tr>' +
-                    '<tr class="cursor-pointer" onclick="newLeads(' +
-                    d.id + ')">' +
-                    '<td><input type="radio" name="similar' + d.id +
-                    '" class="form-check-input item-' + d.id + '" onclick="newLeads(' +
-                    d.id + ')" /></td>' +
-                    '<td colspan=7>New Student</td>' +
-                    '</tr>' +
-                    '</table>'
+                similar += '</table>'
                 // `d` is the original data object for the row
                 return (similar);
             }
@@ -334,11 +343,37 @@
                     'pageLength', {
                         extend: 'excel',
                         text: 'Export to Excel',
-                    }
+                    },
+                    {
+                        text: '<i class="bi bi-check-square me-1"></i> Select All',
+                        action: function(e, dt, node, config) {
+                            selectAll(true);
+                        }
+                    },
+                    {
+                        text: '<i class="bi bi-x-square  me-1"></i> Select None',
+                        action: function(e, dt, node, config) {
+                            selectAll(false);
+                        }
+                    },
+                    {
+                        text: '<i class="bi bi-trash-fill me-1"></i> Delete',
+                        action: function(e, dt, node, config) {
+                            multipleDelete();
+                        }
+                    },
+                ],
+                lengthMenu: [
+                    [10, 50, 100, -1],
+                    ['10 Leads', '50 Leads', '100 Leads', 'Show all']
                 ],
                 scrollX: true,
+                columnDefs: [{
+                    width: 20,
+                    targets: 0
+                }],
                 fixedColumns: {
-                    left: (widthView < 768) ? 1 : 2,
+                    left: (widthView < 768) ? 3 : 4,
                     right: 2
                 },
                 processing: true,
@@ -346,17 +381,23 @@
                 ajax: {
                     url: '',
                 },
+                rowCallback: function(row, data) {
+                    if (data.suggestion) {
+                        $('td:eq(0)', row).addClass('dt-control');
+                    }
+                },
                 columns: [{
-                        className: 'dt-control',
+                        data: null,
                         orderable: false,
                         data: null,
-                        defaultContent: ''
+                        defaultContent: '',
                     },
                     {
                         data: 'id',
                         className: 'text-center',
                         render: function(data, type, row, meta) {
-                            return meta.row + meta.settings._iDisplayStart + 1;
+                            return '<input type="checkbox" class="editor-active cursor-pointer" data-id="' +
+                                data + '">'
                         }
                     },
                     {
@@ -374,7 +415,8 @@
                                 return '-'
                             } else {
                                 var arraySuggestion = data.split(',');
-                                return '<div class="badge badge-warning py-1 px-2 ms-2">' + arraySuggestion.length + ' Similar Names</div>'
+                                return '<div class="badge badge-warning py-1 px-2 ms-2">' +
+                                    arraySuggestion.length + ' Similar Names</div>'
                             }
                         }
                     },
@@ -405,10 +447,14 @@
                             if (data != null) {
                                 if (row.is_verifiedschool == 'Y') {
                                     return data +
-                                        '<div class="badge badge-success py-1 px-2 ms-2">Verified</div>'
+                                        '<i class="bi bi-check-circle-fill text-success ms-1" data-bs-toggle="tooltip" data-bs-placement="top" ' +
+                                        'data-bs-custom-class="custom-tooltip" ' +
+                                        'data-bs-title="Verified"></i>'
                                 } else {
                                     return data +
-                                        '<div class="badge badge-danger py-1 px-2 ms-2">Not Verified</div>'
+                                        '<i class="bi bi-x-circle-fill text-danger ms-1" data-bs-toggle="tooltip" data-bs-placement="top" ' +
+                                        'data-bs-custom-class="custom-tooltip" ' +
+                                        'data-bs-title="Not Verified"></i>'
                                 }
                             } else {
                                 return data
@@ -443,13 +489,28 @@
                     {
                         data: '',
                         className: 'text-center',
-                        defaultContent: '<button type="button" class="btn btn-sm btn-outline-danger py-1 px-2 deleteRawClient"><i class="bi bi-eraser"></i></button>'
+                        defaultContent: '<button type="button" class="btn btn-sm btn-outline-danger py-1 px-2 deleteRawClient"><i class="bi bi-eraser"></i></button>',
+                        render: function(data, type, row, meta) {
+                            return '<div class="d-flex gap-1 justify-content-center">' +
+                                '<small class="btn btn-sm btn-info px-1 pt-1 pb-0  cursor-pointer item-' +
+                                row
+                                .id +
+                                '" data-bs-toggle="tooltip" data-bs-placement="top" ' +
+                                'data-bs-custom-class="custom-tooltip" ' +
+                                'data-bs-title="Convert to New Lead" onclick="newLeads(' +
+                                row.id + ')">' +
+                                '<i class="bi bi-send-check-fill text-secondary"></i>' +
+                                '</small>' +
+                                '<small data-bs-toggle="tooltip" data-bs-placement="top" ' +
+                                'data-bs-custom-class="custom-tooltip" ' +
+                                'data-bs-title="Delete" class="btn btn-sm btn-danger px-1 pt-1 pb-0  cursor-pointer deleteRawClient">' +
+                                '<i class="bi bi-trash"></i>' +
+                                '</small>' +
+                                '</div>';
+                        }
                     },
                 ],
             });
-
-            // realtimeData(table)
-
 
             // Add a click event listener to each row in the parent DataTable
             table.on('click', 'td.dt-control', function(e) {
@@ -485,7 +546,7 @@
                                 swal.close()
                                 console.log(error);
                             })
-                    }else{
+                    } else {
 
                         row.child(format(row.data(), null)).show();
                     }
@@ -497,7 +558,56 @@
                 confirmDelete('client/student/raw', data.id)
             });
 
+            // Tooltip 
+            $('#rawTable tbody').on('mouseover', 'tr', function() {
+                $('[data-bs-toggle="tooltip"]').tooltip({
+                    trigger: 'hover',
+                    html: true
+                });
+            });
+
         });
+
+        function selectAll(condition) {
+            if (condition) {
+                $('input.editor-active').each(function() {
+                    $(this).prop('checked', true)
+                })
+            } else {
+                $('input.editor-active').each(function() {
+                    $(this).prop('checked', false)
+                })
+            }
+        }
+
+        function multipleDelete() {
+            var selected = [];
+            $('input.editor-active').each(function() {
+                if ($(this).prop('checked')) {
+                    selected.push($(this).data('id'));
+                }
+            });
+
+            if (selected.length > 0) {
+                Swal.fire({
+                    title: "Confirmation!",
+                    text: 'Are you sure to delete the school?',
+                    showCancelButton: true,
+                    confirmButtonText: "Yup",
+                }).then((result) => {
+                    /* Read more about isConfirmed, isDenied below */
+                    if (result.isConfirmed) {
+                        console.log(selected);
+                    }
+                });
+            } else {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Please select the school first!",
+                });
+            }
+        }
 
         function comparison(id, id2) {
             $('input.item-' + id2).prop('checked', true);
