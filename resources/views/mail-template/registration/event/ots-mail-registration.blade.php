cc9b8fe78fdbdc8e08a80483c0c5197c5dd4d138
unknown <manuel.eric.b@gmail.com>
2024-02-29 10:52:04 +0700

    feat: updated mail button assessment

--- resources/views/mail-template/registration/event/ots-mail-registration.blade.php
+++ resources/views/mail-template/registration/event/ots-mail-registration.blade.php
@@ -25,7 +25,7 @@
                             </p>
                             <p style="text-align: center">
                                 <a href="{{ $client['assessment_link'] }}">
-                                    <button style="border: 1px solid #0100D4; background-color: #0168FF; margin: 10px 18px;">Take the Test</button>
+                                    <button style="border: 1px solid #0100D4; background-color: #0168FF; padding: 15px 25px;">Take the Test</button>
                                 </a>
                             </p>
                             @endif
