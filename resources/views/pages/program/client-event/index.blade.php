8aaeb74f718d63f1eacad61445c9e364a49ea91b
ahmadfauzan <fauzana566@gmail.com>
2024-05-13 10:57:21 +0700

    Update prod list client event

--- resources/views/pages/program/client-event/index.blade.php
+++ resources/views/pages/program/client-event/index.blade.php
@@ -248,7 +248,7 @@
                         extend: 'excel',
                         text: 'Export to Excel',
                         exportOptions: {
-                            columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+                            columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
                             format: {
                                 body: function(data, row, column, node) {
 
@@ -271,11 +271,11 @@
                                                     /(&nbsp;|<([^>]+)>)/ig, "");
                                             break;
 
-                                        case 17:
+                                        case 18:
                                             return $(data).is("input") ? $(data).val() : data;
                                             break;
 
-                                        case 18:
+                                        case 19:
                                             return $(data).is("input") && $(data).attr('checked') ?
                                                 "✓" : "-";
                                             break;
