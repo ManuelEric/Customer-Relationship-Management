2c8e15c554bfac9d6a3d97fae33d7eb195220ec4
ahmadfauzan <fauzana566@gmail.com>
2024-10-04 14:09:48 +0700

    feat: updated timesheet uses

--- app/Http/Controllers/Api/v1/ExtClientController.php
+++ app/Http/Controllers/Api/v1/ExtClientController.php
@@ -2113,4 +2113,17 @@ class ExtClientController extends Controller
 
         return response()->json($response);
     }
+
+    public function showMentorTutor($uuid)
+    {
+        $user = \App\Models\User::where('uuid', $uuid)->select('password')->first();
+        if ( !$user )
+        {
+            return response()->json([
+                'success' => false,
+                'error' => 'Cannot find the user.'
+            ]);
+        } 
+        return response()->json($user);
+    }
 }
