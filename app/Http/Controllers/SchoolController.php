43720b0b427ffb0ff32960b13c65ce6f8c5118c8
EC2 Default User <ec2-user@ip-10-0-10-67.ap-southeast-3.compute.internal>
2025-02-12 07:12:14 +0000

    minor update school

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -235,7 +235,7 @@ class SchoolController extends Controller
         ]);
 
         $school_id = $request->route('school');
-        dd($school_details);
+	dd($school_details);
 
         DB::beginTransaction();
         try {
