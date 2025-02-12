cd3205e3eb6bc0e7d9113fcf60cede957cfa5d90
unknown <manuel.eric.b@gmail.com>
2025-02-12 15:57:03 +0700

    feat: checking valid mentor with external mentor

--- app/Http/Controllers/Api/v1/ExtClientProgramController.php
+++ app/Http/Controllers/Api/v1/ExtClientProgramController.php
@@ -15,8 +15,8 @@ class ExtClientProgramController extends Controller
         with([
             'client' => function ($query) {
                 $query->
-                    select('id', 'sch_id', 'first_name', 'last_name')->
-                    selectRaw('UpdateGradeStudent (year(CURDATE()),year(created_at),month(CURDATE()),month(created_at),st_grade) as grade');
+                    select('id', 'sch_id', 'first_name', 'last_name', 'grade_now');
+                    // selectRaw('UpdateGradeStudent (year(CURDATE()),year(created_at),month(CURDATE()),month(created_at),st_grade) as grade');
             },
             'client.school' => function ($query) {
                 $query->select('sch_id', 'sch_name');
@@ -38,7 +38,7 @@ class ExtClientProgramController extends Controller
             $client_id = $data->client->id;
             $client_fname = $data->client->first_name;
             $client_lname = $data->client->last_name;
-            $client_grade = $data->client->grade;
+            $client_grade = $data->client->grade_now;
             $school_name = $data->client->school ? $data->client->school->sch_name : null;
 
             return [
