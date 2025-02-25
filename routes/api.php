baa52011a46e1b94086de75deb423c6889e3221d
unknown <manuel.eric.b@gmail.com>
2025-02-25 10:28:40 +0700

    updated api route editing

--- routes/api.php
+++ routes/api.php
@@ -190,11 +190,11 @@ Route::prefix('v1')->group(function () {
     # essay editing & timesheet API use
     Route::middleware(['resource:timesheet,editing'])->group(function () {
         Route::get('auth/email/check', [ExtClientController::class, 'checkUserEmail']);
+        Route::post('auth/token', [ExtClientController::class, 'validateCredentials']);
     });
 
     # timesheet
     Route::middleware(['resource:timesheet'])->group(function () {
-        Route::post('auth/token', [ExtClientController::class, 'validateCredentials']);
         Route::post('user/update', [ExtClientController::class, 'updateUser']);
 
         Route::get('user/mentor-tutors', [ExtClientController::class, 'getMentorTutors']);
