25e98fe0838e0065c376ee09d1ec32d5b7ab9c6d
ahmadfauzan <fauzana566@gmail.com>
2025-03-19 10:41:54 +0700

    Send bot tele when have error

---
+++ app/Logging/AddInstructionsProcessor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Logging;
+
+use App\Http\Traits\SendBotTelegramTrait;
+use Illuminate\Support\Facades\Log;
+
+class AddInstructionsProcessor
+{
+    use SendBotTelegramTrait;
+    public function __invoke($record)
+    {
+        if ($record['level_name'] === 'ERROR') {
+            $record['extra']['instructions'] = $this->SendMessageTele(env("TOKEN_BOT_TELEGRAM"), env("TELE_ID"), 'log-error', $record['message']);
+        }
+
+        return $record;
+    }
+
+}
