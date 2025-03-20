d7946bb89c609c1c42824ee0b848880559efc683
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2025-03-20 11:41:06 +0700

    comment telegram

--- app/Logging/AddInstructionsProcessor.php
+++ app/Logging/AddInstructionsProcessor.php
@@ -11,7 +11,7 @@ class AddInstructionsProcessor
     public function __invoke($record)
     {
         if ($record['level_name'] === 'ERROR') {
-            $record['extra']['instructions'] = $this->SendMessageTele(env("TOKEN_BOT_TELEGRAM"), env("TELE_ID"), 'log-error', $record['message']);
+            // $record['extra']['instructions'] = $this->SendMessageTele(env("TOKEN_BOT_TELEGRAM"), env("TELE_ID"), 'log-error', $record['message']);
         }
 
         return $record;
