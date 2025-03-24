886a5e7d0e053fd39aedba91155eb94bdd926e9a
all-inedu <ec2-user@ip-10-0-10-67.ap-southeast-3.compute.internal>
2025-03-24 01:47:29 +0000

    push auth controller update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -39,7 +39,7 @@ class AuthController extends Controller
 
         # check credentials
         if (!Auth::attempt($credentials, true)) 
-            return back()->withErrors([ 'password' => 'Wrong email or password' ]);
+            return back()->withError([ 'password' => 'Wrong email or password' ]);
 
         
         try {
