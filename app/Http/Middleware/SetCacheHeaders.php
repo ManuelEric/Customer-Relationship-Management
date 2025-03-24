df616cfe83dc9e99c8c66381f7ff59d5eb33ff5e
ahmadfauzan <fauzana566@gmail.com>
2025-03-24 14:09:18 +0700

    Update cache middleware

--- app/Http/Middleware/SetCacheHeaders.php
+++ app/Http/Middleware/SetCacheHeaders.php
@@ -19,8 +19,11 @@ class SetCacheHeaders
         $response = $next($request);
 
         // Set Cache-Control headers
-        $response->header('Cache-Control', 'public, max-age=604800'); // 1 week (604800 seconds)
-
+        $response->header('Cache-Control', 'no-store,no-cache, must-revalidate, post-check=0, pre-check=0, max-age=30'); // 1 week (604800 seconds)
+        
+        # old
+        // $response->header('Cache-Control', 'public, max-age=604800'); // 1 week (604800 seconds)
+        
         return $response;
     }
 }
