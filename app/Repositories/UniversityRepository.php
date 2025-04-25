408166052097af4c641fc9b12df25351e3af92cb
unknown <manuel.eric.b@gmail.com>
2025-04-25 15:36:14 +0700

    revise order by uni app

--- app/Repositories/UniversityRepository.php
+++ app/Repositories/UniversityRepository.php
@@ -105,7 +105,7 @@ class UniversityRepository implements UniversityRepositoryInterface
         return University::search($search)->
             whereBetween('early_action', [Carbon::now()->format('Y-m-d') . ' 00:00:00', Carbon::now()->addDays(30)->format('Y-m-d') . ' 23:59:59'])->
             orWhereBetween('early_decision', [Carbon::now()->format('Y-m-d') . ' 00:00:00', Carbon::now()->addDays(30)->format('Y-m-d') . ' 23:59:59'])->
-            orWhereBetween('regular_deadline', [Carbon::now()->format('Y-m-d') . ' 00:00:00', Carbon::now()->addDays(30)->format('Y-m-d') . ' 23:59:59']);
+            orWhereBetween('regular_deadline', [Carbon::now()->format('Y-m-d') . ' 00:00:00', Carbon::now()->addDays(30)->format('Y-m-d') . ' 23:59:59'])->get();
             // orderBy('early', 'asc')->get();
     }
 
