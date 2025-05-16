d9038981648ab92ff485531a605a7c958989951d
ManuelEric <79891798+ManuelEric@users.noreply.github.com>
2025-05-16 10:25:50 +0700

    fix format rupiah

--- app/Http/Traits/RupiahFormatterTrait.php
+++ app/Http/Traits/RupiahFormatterTrait.php
@@ -7,6 +7,6 @@ trait RupiahFormatterTrait
     public function formatRupiah($amount)
     {
         // Format the amount as a string with thousands separator and currency symbol
-        return 'Rp ' . number_format($amount, 0, ',', '.');
+        return 'Rp. ' . number_format($amount, 0, ',', '.');
     }
 }
