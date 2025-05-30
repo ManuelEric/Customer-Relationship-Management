0ac71601b32dbff01a39088de917d1ee2865fba9
ManuelEric <79891798+ManuelEric@users.noreply.github.com>
2025-05-30 21:43:20 +0700

    fix problem program name

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -178,7 +178,7 @@ class Program extends Model
     {
         return $query->whereHas('sub_prog', function ($query) {
             $query->where('sub_prog_name', 'like', '%Tutoring%')->orWhere('sub_prog_name', 'like', '%Competition%');
-        })->orWhereRelation('main_prog', 'prog_name', 'Test Preparation');
+        })->orWhereRelation('main_prog', 'prog_name', 'Test Preparation')->whereNot('prog_id', 'SATPREP');
     }
 
     public function scopeSubjectTutoringProgList($query)
