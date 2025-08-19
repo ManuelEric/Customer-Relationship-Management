8087a320eac354eddf1760f5dc942290b21f1e95
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2025-08-19 11:05:43 +0700

    update logstash

--- config/logging.php
+++ config/logging.php
@@ -69,7 +69,6 @@ return [
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 365,
             'permission' => 0777,
-            'tap' => [\App\Logging\LogInjector::class]
         ],
 
         'slack' => [
