e22ce07ad0d5e354c8ad9356cd45bfc6ce8a229b
ManuelEric <79891798+ManuelEric@users.noreply.github.com>
2025-09-04 14:02:02 +0700

    fix: disable log route change

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
 	        \Illuminate\Http\Middleware\SetCacheHeaders::class,
-            \App\Http\Middleware\LogRouteChange::class,
+            // \App\Http\Middleware\LogRouteChange::class,
         ],
 
         'api' => [
